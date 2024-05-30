75c238d527ebc46b215a73a88beaae66878b2d44
JosuePlataV <144969668+JosuePlataV@users.noreply.github.com>
2024-05-29 19:38:24 -0500

    Se agregó de la API
    Se agregó información de los characters de la API

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const container = document.querySelector("#container")
+const url = "https://rickandmortyapi.com/api" 
+
+fetch(url + "/character") 
+    .then(response => response.json())
+    .then(json => {
+        const characters = json.results
+        characters.forEach(character => {
+            container.innerHTML += character.name
+        })
+    })
