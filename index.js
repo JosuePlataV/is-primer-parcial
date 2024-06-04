79d34c94c7d6fdca2b190b1a82f79f7577ff18de
JosuePlataV <144969668+JosuePlataV@users.noreply.github.com>
2024-06-03 19:21:13 -0500

    Se interpoló el api con el documento

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ fetch(url + "/character")
     .then(json => {
         const characters = json.results
         characters.forEach(character => {
-            container.innerHTML += character.name
+            const characterTemplate = `
+        <div class="character">
+            <img src="${character.image}" alt="">
+            <div class="character-detail">
+                <h1>${character.name} </h1>
+                <h2>${character.status} - ${character.species}</h2>
+                <p>Last known location:</p>
+                <p>${character.origin.name}</p>
+                <p>First seen in:</p>
+                <p>${character.location.name}</p>
+            </div>
+        </div>`
+            container.innerHTML += characterTemplate
         })
     })
