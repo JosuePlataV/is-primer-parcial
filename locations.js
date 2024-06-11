32367bda12f738efdb1cc38b8132e7e5b1143c4c
JosuePlataV <144969668+JosuePlataV@users.noreply.github.com>
2024-06-10 20:32:26 -0500

    Update locations.js

--- locations.js
+++ locations.js
@@ -8,13 +8,13 @@ fetch(url + "/location")
         locations.forEach(locationz => {
             const locationsTemplatez = `
         <div class="locations">
-            <a href="location.html?id=${locationz.id}">
                 <div class="detailz">
                 <h1>${locationz.name}</h1>
                 <h3>Type:</h3>
                 <p>${locationz.type}</p>
                 <h3>Dimension:</h3>
                 <p>${locationz.dimension}</p>
+                <a href="location.html?id=${locationz.id}">
                 <h3>URL:</h3>
                 <p>${locationz.url}</p>
             </div>
